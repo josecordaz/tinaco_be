9efca9623492648888603c0a96e682482af4e072
JOSE CARLOS ORDAZ CRIZANTOS <josecordaz@gmail.com>
2018-05-02 09:04:23 -0500

    Fixing performance

--- main.go
+++ main.go
@@ -1,103 +1,194 @@
 package main
 
-// import (
-// 	"encoding/json"
-// 	"fmt"
-// 	"log"
-// 	"math/rand"
-// 	"net/http"
-// 	"os"
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"os"
+	"time"
 
-// 	"github.com/gorilla/handlers"
-// 	"github.com/gorilla/mux"
-// 	rpio "github.com/stianeikeland/go-rpio"
-// )
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	rpio "github.com/stianeikeland/go-rpio"
+)
 
-// var (
-// 	// Use mcu pin 10, corresponds to physical pin 19 on the pi
-// 	bombPin = rpio.Pin(17)
-// )
+var (
+	// Use mcu pin 10, corresponds to physical pin 19 on the pi
+	bombPin = rpio.Pin(17)
+	trigPin = rpio.Pin(23)
+	echoPin = rpio.Pin(24)
+)
 
-// var bomba_state = false
+var bomba_state = false
 
-// // The person Type (more like an object)
-// type Person struct {
-// 	ID        string   `json:"id,omitempty"`
-// 	Firstname string   `json:"firstname,omitempty"`
-// 	Lastname  string   `json:"lastname,omitempty"`
-// 	Address   *Address `json:"address,omitempty"`
-// }
+var cont int64
 
-// type Address struct {
-// 	City  string `json:"city,omitempty"`
-// 	State string `json:"state,omitempty"`
-// }
+type Conn struct {
+	Status bool `json:"status,omitempty"`
+}
+type Level struct {
+	Level int `json:"level,omitempty"`
+}
 
-// type Conn struct {
-// 	Status bool `json:"status,omitempty"`
-// }
-// type Level struct {
-// 	Level int `json:"level,omitempty"`
-// }
+func Swap(arrayzor []float64, i, j int) {
+	tmp := arrayzor[j]
+	arrayzor[j] = arrayzor[i]
+	arrayzor[i] = tmp
+}
 
-// // Display all from the people var
-// func GetConn(w http.ResponseWriter, r *http.Request) {
-// 	conn := Conn{rand.Intn(2) == 1}
-// 	json.NewEncoder(w).Encode(conn)
-// }
+func bubbleSort(arrayzor []float64) {
+	swapped := true
+	for swapped {
+		swapped = false
+		for i := 0; i < len(arrayzor)-1; i++ {
+			if arrayzor[i+1] < arrayzor[i] {
+				Swap(arrayzor, i, i+1)
+				swapped = true
+			}
+		}
+	}
+}
 
-// func GetLevel(w http.ResponseWriter, r *http.Request) {
-// 	level := Level{rand.Intn(100)}
-// 	json.NewEncoder(w).Encode(level)
-// }
+func getMeasurement() float64 {
+	// GPIO.setup(TRIG,GPIO.OUT)
+	trigPin.Output()
 
-// func GetBombStatus(w http.ResponseWriter, r *http.Request) {
-// 	// conn := Conn{rand.Intn(2) == 1}
-// 	conn := Conn{bomba_state}
-// 	json.NewEncoder(w).Encode(conn)
-// }
+	// GPIO.output(TRIG,0)
+	trigPin.Low()
 
-// func TurnBombOn(w http.ResponseWriter, r *http.Request) {
-// 	bombPin.Low()
-// 	bomba_state = true
-// 	json.NewEncoder(w).Encode(Conn{true})
-// }
+	// GPIO.setup(ECHO,GPIO.IN)
+	echoPin.Input()
 
-// func TurnBombOff(w http.ResponseWriter, r *http.Request) {
-// 	bombPin.High()
-// 	bomba_state = false
-// 	json.NewEncoder(w).Encode(Conn{true})
-// }
+	// time.sleep(0.1)
+	time.Sleep(time.Millisecond * 100)
 
-// func checkLevelWater() {
-// 	// Here we will have to read from ultrasensor
-// 	// and it will update all the variables and states
-// 	// and if the level is lower than limit it will turn the bomb on
-// }
+	// GPIO.output(TRIG, 1)
+	trigPin.High()
 
-// // main function to boot up everything
-// func main() {
-// 	router := mux.NewRouter()
-// 	router.HandleFunc("/con", GetConn).Methods("GET")
-// 	router.HandleFunc("/level", GetLevel).Methods("GET")
-// 	router.HandleFunc("/b_on", TurnBombOn).Methods("GET")
-// 	router.HandleFunc("/b_off", TurnBombOff).Methods("GET")
-// 	router.HandleFunc("/b_status", GetBombStatus).Methods("GET")
-// 	router.Headers("Access-Control-Allow-Origin", "*")
+	// time.sleep(0.00001)
+	time.Sleep(time.Microsecond * 10)
 
-// 	// Open and map memory to access gpio, check for errors
-// 	if err := rpio.Open(); err != nil {
-// 		fmt.Println(err)
-// 		os.Exit(1)
-// 	}
+	// GPIO.output(TRIG, 0)
+	trigPin.Low()
 
-// 	// Unmap gpio memory when done
-// 	defer rpio.Close()
+	// while GPIO.input(ECHO) == 0:
+	//     pass
+	// start = time.time()
+	for echoPin.Read() == rpio.Low {
+	}
+	start := time.Now()
 
-// 	// Set pin to output mode
-// 	bombPin.Output()
+	// while GPIO.input(ECHO) == 1:
+	//     pass
+	// stop = time.time()
+	for echoPin.Read() == rpio.High {
+	}
+	d := time.Since(start)
 
-// 	fmt.Println("Server ready!!")
-// 	go checkLevelWater()
-// 	log.Fatal(http.ListenAndServe(":8000", handlers.CORS()(router)))
-// }
+	res := d.Seconds() * 17000
+
+	// fmt.Println("Distancia ", res)
+	return res
+}
+
+func getDistance() float64 {
+	ds := make([]float64, 0)
+	var avg, sum float64
+	for i := 0; i < 10; i++ {
+		ds = append(ds, getMeasurement())
+	}
+
+	bubbleSort(ds)
+
+	ds = ds[2:7]
+
+	for _, v := range ds {
+		sum += v
+	}
+
+	avg = sum / 5
+
+	return avg
+}
+
+func convertToPCT(d float64) int {
+	low := 5.5
+	hight := 22.5
+
+	if d < low {
+		return 100
+	} else if d > hight {
+		return 0
+	}
+
+	rdistance := d - low
+	total := hight - low
+
+	tmp := ((rdistance * 100) / total)
+	fmt.Println("tmp", tmp)
+
+	return int(100 - ((rdistance * 100) / total))
+}
+
+// Display all from the people var
+func GetConn(w http.ResponseWriter, r *http.Request) {
+	conn := Conn{rand.Intn(2) == 1}
+	json.NewEncoder(w).Encode(conn)
+}
+
+func GetLevel(w http.ResponseWriter, r *http.Request) {
+	d := getDistance()
+	level := convertToPCT(d)
+	json.NewEncoder(w).Encode(Level{level})
+	cont++
+	fmt.Println(cont, " Level: ", level)
+}
+
+func GetBombStatus(w http.ResponseWriter, r *http.Request) {
+	// conn := Conn{rand.Intn(2) == 1}
+	conn := Conn{bomba_state}
+	json.NewEncoder(w).Encode(conn)
+}
+
+func TurnBombOn(w http.ResponseWriter, r *http.Request) {
+	bombPin.Low()
+	bomba_state = true
+	json.NewEncoder(w).Encode(Conn{true})
+}
+
+func TurnBombOff(w http.ResponseWriter, r *http.Request) {
+	bombPin.High()
+	bomba_state = false
+	json.NewEncoder(w).Encode(Conn{true})
+}
+
+// main function to boot up everything
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/con", GetConn).Methods("GET")
+	router.HandleFunc("/level", GetLevel).Methods("GET")
+	router.HandleFunc("/b_on", TurnBombOn).Methods("GET")
+	router.HandleFunc("/b_off", TurnBombOff).Methods("GET")
+	router.HandleFunc("/b_status", GetBombStatus).Methods("GET")
+	router.Headers("Access-Control-Allow-Origin", "*")
+
+	// Open and map memory to access gpio, check for errors
+	if err := rpio.Open(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// Unmap gpio memory when done
+	defer rpio.Close()
+
+	// Set pin to output mode
+	bombPin.Output()
+
+	fmt.Println("Server ready!!")
+	err := http.ListenAndServe(":8000", handlers.CORS()(router))
+	if err != nil {
+		fmt.Println("Error", err)
+	}
+
+}
--- sensor.go
+++ sensor.go
@@ -1,106 +1,162 @@
-// import RPi.GPIO as GPIO
-// import time
+// // import RPi.GPIO as GPIO
+// // import time
 
-// GPIO.setmode(GPIO.BCM)
+// // GPIO.setmode(GPIO.BCM)
 
-// TRIG = 23
-// ECHO = 24
+// // TRIG = 23
+// // ECHO = 24
 
-// print "Distance Measurement In Progress"
+// // print "Distance Measurement In Progress"
 
-// GPIO.setup(TRIG,GPIO.OUT)
-// GPIO.output(TRIG,0)
+// // GPIO.setup(TRIG,GPIO.OUT)
+// // GPIO.output(TRIG,0)
 
-// GPIO.setup(ECHO,GPIO.IN)
+// // GPIO.setup(ECHO,GPIO.IN)
 
-// time.sleep(0.1)
+// // time.sleep(0.1)
 
-// print "Stargin measurement"
+// // print "Stargin measurement"
 
-// GPIO.output(TRIG, 1)
-// time.sleep(0.00001)
-// GPIO.output(TRIG, 0)
+// // GPIO.output(TRIG, 1)
+// // time.sleep(0.00001)
+// // GPIO.output(TRIG, 0)
 
-// while GPIO.input(ECHO) == 0:
-//     pass
-// start = time.time()
+// // while GPIO.input(ECHO) == 0:
+// //     pass
+// // start = time.time()
 
-// while GPIO.input(ECHO) == 1:
-//     pass
-// stop = time.time()
+// // while GPIO.input(ECHO) == 1:
+// //     pass
+// // stop = time.time()
 
-// print (stop - start) * 17000
+// // print (stop - start) * 17000
 
 package main
 
-import (
-	"fmt"
-	"os"
-	"time"
+// import (
+// 	"fmt"
+// 	"os"
+// 	"time"
 
-	rpio "github.com/stianeikeland/go-rpio"
-)
+// 	rpio "github.com/stianeikeland/go-rpio"
+// )
 
-var (
-	// Use mcu pin 10, corresponds to physical pin 19 on the pi
-	trigPin = rpio.Pin(23)
-	echoPin = rpio.Pin(24)
-)
+// var (
+// 	// Use mcu pin 10, corresponds to physical pin 19 on the pi
+// 	trigPin = rpio.Pin(23)
+// 	echoPin = rpio.Pin(24)
+// )
 
-func getMeasurement() float64 {
-	// GPIO.setup(TRIG,GPIO.OUT)
-	trigPin.Output()
+// func Swap(arrayzor []float64, i, j int) {
+// 	tmp := arrayzor[j]
+// 	arrayzor[j] = arrayzor[i]
+// 	arrayzor[i] = tmp
+// }
 
-	// GPIO.output(TRIG,0)
-	trigPin.Low()
+// func bubbleSort(arrayzor []float64) {
+// 	swapped := true
+// 	for swapped {
+// 		swapped = false
+// 		for i := 0; i < len(arrayzor)-1; i++ {
+// 			if arrayzor[i+1] < arrayzor[i] {
+// 				Swap(arrayzor, i, i+1)
+// 				swapped = true
+// 			}
+// 		}
+// 	}
+// }
 
-	// GPIO.setup(ECHO,GPIO.IN)
-	echoPin.Input()
+// func getMeasurement() float64 {
+// 	// GPIO.setup(TRIG,GPIO.OUT)
+// 	trigPin.Output()
 
-	// time.sleep(0.1)
-	time.Sleep(time.Millisecond * 100)
+// 	// GPIO.output(TRIG,0)
+// 	trigPin.Low()
 
-	// GPIO.output(TRIG, 1)
-	trigPin.High()
+// 	// GPIO.setup(ECHO,GPIO.IN)
+// 	echoPin.Input()
 
-	// time.sleep(0.00001)
-	time.Sleep(time.Microsecond * 10)
+// 	// time.sleep(0.1)
+// 	time.Sleep(time.Millisecond * 100)
 
-	// GPIO.output(TRIG, 0)
-	trigPin.Low()
+// 	// GPIO.output(TRIG, 1)
+// 	trigPin.High()
 
-	// while GPIO.input(ECHO) == 0:
-	//     pass
-	// start = time.time()
-	for echoPin.Read() == rpio.Low {
-	}
-	start := time.Now()
+// 	// time.sleep(0.00001)
+// 	time.Sleep(time.Microsecond * 10)
 
-	// while GPIO.input(ECHO) == 1:
-	//     pass
-	// stop = time.time()
-	for echoPin.Read() == rpio.High {
-	}
-	d := time.Since(start)
+// 	// GPIO.output(TRIG, 0)
+// 	trigPin.Low()
 
-	res := d.Seconds() * 17000
+// 	// while GPIO.input(ECHO) == 0:
+// 	//     pass
+// 	// start = time.time()
+// 	for echoPin.Read() == rpio.Low {
+// 	}
+// 	start := time.Now()
 
-	// fmt.Println("Distancia ", res)
-	return res
-}
+// 	// while GPIO.input(ECHO) == 1:
+// 	//     pass
+// 	// stop = time.time()
+// 	for echoPin.Read() == rpio.High {
+// 	}
+// 	d := time.Since(start)
 
-func main() {
-	// Open and map memory to access gpio, check for errors
-	if err := rpio.Open(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+// 	res := d.Seconds() * 17000
 
-	for i := 0; i < 10; i++ {
-		fmt.Println("Distance", getMeasurement())
-	}
+// 	// fmt.Println("Distancia ", res)
+// 	return res
+// }
 
-	// Unmap gpio memory when done
-	defer rpio.Close()
+// func getDistance() float64 {
+// 	ds := make([]float64, 0)
+// 	var avg, sum float64
+// 	for i := 0; i < 10; i++ {
+// 		ds = append(ds, getMeasurement())
+// 	}
 
-}
+// 	bubbleSort(ds)
+
+// 	ds = ds[2:7]
+
+// 	for _, v := range ds {
+// 		sum += v
+// 	}
+
+// 	avg = sum / 5
+
+// 	return avg
+// }
+
+// func convertToPCT(d float64) int {
+// 	low := 3.5
+// 	hight := 22.5
+
+// 	if d < low {
+// 		return 100
+// 	} else if d > hight {
+// 		return 0
+// 	}
+
+// 	rdistance := d - low
+// 	total := hight - low
+
+// 	return int(100 - ((rdistance * 100) / total))
+// }
+
+// func main() {
+// 	// Open and map memory to access gpio, check for errors
+// 	if err := rpio.Open(); err != nil {
+// 		fmt.Println(err)
+// 		os.Exit(1)
+// 	}
+
+// 	d := getDistance()
+
+// 	fmt.Println("Distance", d)
+// 	fmt.Println("Percentage", convertToPCT(d))
+
+// 	// Unmap gpio memory when done
+// 	defer rpio.Close()
+
+// }

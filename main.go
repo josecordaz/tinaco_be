c1e1e0888f45b6dfa32623339a8aed46943d7c21
JOSE CARLOS ORDAZ CRIZANTOS <josecordaz@gmail.com>
2018-05-11 16:32:01 -0500

    Added jwt

--- main.go
+++ main.go
@@ -2,23 +2,36 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
-	"os"
-	"sync"
+	"strings"
 	"time"
 
-	"github.com/gorilla/handlers"
-	"github.com/gorilla/mux"
-	rpio "github.com/stianeikeland/go-rpio"
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/rs/cors"
+	// rpio "github.com/stianeikeland/go-rpio"
 )
 
-var (
-	// Use mcu pin 10, corresponds to physical pin 19 on the pi
-	bombPin = rpio.Pin(17)
-	trigPin = rpio.Pin(23)
-	echoPin = rpio.Pin(24)
+const (
+	SECRET = "42isTheAnswer"
 )
+
+type JWTData struct {
+	// Standard claims are the standard jwt claims from the IETF standard
+	// https://tools.ietf.org/html/rfc7519
+	jwt.StandardClaims
+	CustomClaims map[string]string `json:"custom,omitempty"`
+}
+
+// var (
+// 	// Use mcu pin 10, corresponds to physical pin 19 on the pi
+// 	bombPin = rpio.Pin(17)
+// 	trigPin = rpio.Pin(23)
+// 	echoPin = rpio.Pin(24)
+// )
 
 var bomba_state = false
 
@@ -52,60 +65,60 @@ func bubbleSort(arrayzor []float64) {
 	}
 }
 
-func getMeasurement() float64 {
+// func getMeasurement() float64 {
 
-	trigPin.Low()
+// 	trigPin.Low()
 
-	time.Sleep(time.Millisecond * 200)
+// 	time.Sleep(time.Millisecond * 200)
 
-	trigPin.High()
+// 	trigPin.High()
 
-	time.Sleep(time.Microsecond * 20)
+// 	time.Sleep(time.Microsecond * 20)
 
-	trigPin.Low()
+// 	trigPin.Low()
 
-	var wg sync.WaitGroup
-	var start time.Time
-	var d time.Duration
+// 	var wg sync.WaitGroup
+// 	var start time.Time
+// 	var d time.Duration
 
-	wg.Add(1)
-	go func() {
-		for echoPin.Read() == rpio.Low {
-		}
-		start = time.Now()
-		for echoPin.Read() == rpio.High {
-		}
-		d = time.Since(start)
-		wg.Done()
-	}()
+// 	wg.Add(1)
+// 	go func() {
+// 		for echoPin.Read() == rpio.Low {
+// 		}
+// 		start = time.Now()
+// 		for echoPin.Read() == rpio.High {
+// 		}
+// 		d = time.Since(start)
+// 		wg.Done()
+// 	}()
 
-	wg.Wait()
+// 	wg.Wait()
 
-	res := d.Seconds() * 17000
+// 	res := d.Seconds() * 17000
 
-	return res
-}
+// 	return res
+// }
 
-func getDistance() float64 {
-	ds := make([]float64, 0)
-	var avg, sum float64
-	for i := 0; i < 10; i++ {
-		ds = append(ds, getMeasurement())
-		time.Sleep(time.Millisecond * 100)
-	}
+// func getDistance() float64 {
+// 	ds := make([]float64, 0)
+// 	var avg, sum float64
+// 	for i := 0; i < 10; i++ {
+// 		ds = append(ds, getMeasurement())
+// 		time.Sleep(time.Millisecond * 100)
+// 	}
 
-	bubbleSort(ds)
+// 	bubbleSort(ds)
 
-	ds = ds[2:7]
+// 	ds = ds[2:7]
 
-	for _, v := range ds {
-		sum += v
-	}
+// 	for _, v := range ds {
+// 		sum += v
+// 	}
 
-	avg = sum / 5
+// 	avg = sum / 5
 
-	return avg
-}
+// 	return avg
+// }
 
 func convertToPCT(d float64) int {
 	low := 20.0
@@ -123,11 +136,12 @@ func convertToPCT(d float64) int {
 	return int(100 - ((rdistance * 100) / total))
 }
 
-func GetLevel(w http.ResponseWriter, r *http.Request) {
-	d := getDistance()
-	level := convertToPCT(d)
+func getlevel(w http.ResponseWriter, r *http.Request) {
+	validateRequest(w, r)
+	// d := getDistance()
+	// level := convertToPCT(d)
 	// level.mu.Lock()
-	json.NewEncoder(w).Encode(Level{level})
+	json.NewEncoder(w).Encode(Level{34})
 	// level.mu.Unlock()
 }
 
@@ -138,38 +152,86 @@ func GetBombStatus(w http.ResponseWriter, r *http.Request) {
 }
 
 func TurnBombOn(w http.ResponseWriter, r *http.Request) {
-	bombPin.Low()
+	// bombPin.Low()
 	bomba_state = true
 	json.NewEncoder(w).Encode(Conn{true})
 }
 
 func TurnBombOff(w http.ResponseWriter, r *http.Request) {
-	bombPin.High()
+	// bombPin.High()
 	bomba_state = false
 	json.NewEncoder(w).Encode(Conn{false})
 }
 
+func login(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Login failed!", http.StatusUnauthorized)
+	}
+
+	var userData map[string]string
+	json.Unmarshal(body, &userData)
+
+	// Demo - in real case scenario you'd check this against your database
+	if userData["email"] == "admin@gmail.com" && userData["password"] == "admin123" {
+		claims := JWTData{
+			StandardClaims: jwt.StandardClaims{
+				ExpiresAt: time.Now().Add(time.Hour).Unix(),
+			},
+
+			CustomClaims: map[string]string{
+				"userid": "u1",
+			},
+		}
+
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+		tokenString, err := token.SignedString([]byte(SECRET))
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "Login failed!", http.StatusUnauthorized)
+		}
+
+		json, err := json.Marshal(struct {
+			Token string `json:"token"`
+		}{
+			tokenString,
+		})
+
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "Login failed!", http.StatusUnauthorized)
+		}
+
+		w.Write(json)
+	} else {
+		http.Error(w, "Login failed!", http.StatusUnauthorized)
+	}
+}
+
 // main function to boot up everything
 func main() {
-	router := mux.NewRouter()
+	// router := mux.NewRouter()
+	mux := http.NewServeMux()
 	// router.HandleFunc("/con", GetConn).Methods("GET")
-	router.HandleFunc("/level", GetLevel).Methods("GET")
-	router.HandleFunc("/b_on", TurnBombOn).Methods("GET")
-	router.HandleFunc("/b_off", TurnBombOff).Methods("GET")
-	router.HandleFunc("/b_status", GetBombStatus).Methods("GET")
-	router.Headers("Access-Control-Allow-Origin", "*")
+	mux.HandleFunc("/login", login)
+	mux.HandleFunc("/level", getlevel)
+	// router.HandleFunc("/b_on", TurnBombOn).Methods("GET")
+	// router.HandleFunc("/b_off", TurnBombOff).Methods("GET")
+	// router.HandleFunc("/b_status", GetBombStatus).Methods("GET")
+	// mux.Headers("Access-Control-Allow-Origin", "*")
 
 	// Set pin to output mode
 
-	if err := rpio.Open(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	// if err := rpio.Open(); err != nil {
+	// 	fmt.Println(err)
+	// 	os.Exit(1)
+	// }
 
-	trigPin.Output()
+	// trigPin.Output()
 
-	echoPin.Input()
-	bombPin.Output()
+	// echoPin.Input()
+	// bombPin.Output()
 
 	// Open and map memory to access gpio, check for errors
 
@@ -190,13 +252,59 @@ func main() {
 	// }()
 
 	fmt.Println("Server ready!!")
-	err := http.ListenAndServeTLS("0.0.0.0:443", "certificate.crt", "private.key", handlers.CORS()(router))
-	if err != nil {
-		fmt.Println("Error", err)
-	}
+	// err := http.ListenAndServeTLS("0.0.0.0:443", "certificate.crt", "private.key", handlers.CORS()(router))
+	// err := http.ListenAndServe("0.0.0.0:8082", handlers.CORS()(router))
+	// if err != nil {
+	// 	fmt.Println("Error", err)
+	// }
+
+	// mux.HandleFunc("/", hello)
+	// mux.HandleFunc("/login", login)
+	// mux.HandleFunc("/account", account)
+
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:8100", "https://tinaco2.tk"},
+		AllowCredentials: true,
+		// Enable Debugging for testing, consider disabling in production
+		Debug: true,
+	})
+
+	// Insert the middleware
+	handler := c.Handler(mux)
+
+	// handler := cors.Default().Handler(mux)
+
+	log.Println("Listening for connections on port: ", 8082)
+	log.Fatal(http.ListenAndServe(":8082", handler))
 
 	// wg.Wait()
 
-	rpio.Close()
+	// rpio.Close()
 
+}
+
+func validateRequest(w http.ResponseWriter, r *http.Request) *JWTData {
+	authToken := r.Header.Get("Authorization")
+	authArr := strings.Split(authToken, " ")
+
+	if len(authArr) != 2 {
+		log.Println("Authentication header is invalid: " + authToken)
+		http.Error(w, "Request failed!", http.StatusUnauthorized)
+	}
+
+	jwtToken := authArr[1]
+
+	claims, err := jwt.ParseWithClaims(jwtToken, &JWTData{}, func(token *jwt.Token) (interface{}, error) {
+		if jwt.SigningMethodHS256 != token.Method {
+			return nil, errors.New("Invalid signing algorithm")
+		}
+		return []byte(SECRET), nil
+	})
+
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Request failed!", http.StatusUnauthorized)
+	}
+
+	return claims.Claims.(*JWTData)
 }

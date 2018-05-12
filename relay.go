d201ccea3a8f74f98a08c5b5361b04fe360ae66c
JOSE CARLOS ORDAZ CRIZANTOS <josecordaz@gmail.com>
2018-05-12 11:21:57 -0500

    relay test

---
+++ relay.go
@@ -0,0 +1,29 @@
+// rpio "github.com/stianeikeland/go-rpio"
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	rpio "github.com/stianeikeland/go-rpio"
+)
+
+var (
+	bombPin = rpio.Pin(27)
+)
+
+func main() {
+	if err := rpio.Open(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	bombPin.Output()
+
+	bombPin.High()
+
+	time.Sleep(time.Second * 5)
+
+	rpio.Close()
+}

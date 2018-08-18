cb4a3127a37b316d8b6c99e12c6e2c3f09b37f63
JOSE CARLOS ORDAZ CRIZANTOS <josecordaz@gmail.com>
2018-08-17 20:42:55 -0500

    fixing

--- tinaco.py
+++ tinaco.py
@@ -15,7 +15,36 @@ def hello():
 
 
 def getMeasure():
-    return 45
+    print "Distance Measurement In Progress"
+
+    GPIO.setup(TRIG,GPIO.OUT)
+    GPIO.output(TRIG,0)
+
+    GPIO.setup(ECHO,GPIO.IN)
+
+    time.sleep(0.1)
+
+    print "Stargin measurement"
+
+    GPIO.output(TRIG, 1)
+    time.sleep(0.00001)
+    GPIO.output(TRIG, 0)
+
+    while GPIO.input(ECHO) == 0:
+        pass
+    start = time.time()
+
+    while GPIO.input(ECHO) == 1:
+        pass
+    stop = time.time()
+
+    print (stop - start)
+
+    print (stop - start) * 17000
+
+    GPIO.cleanup()
+
+    return (stop - start) * 17000
 
 if __name__ == '__main__':
     app.run(host= '0.0.0.0',debug=True)
